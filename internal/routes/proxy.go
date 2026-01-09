ac1b6039caa60801c4552df262aaf6e1a1d2ae1a
Max Celant <maxcelant@gmail.com>
2026-01-08 21:54:54 -0600

    fix: update to set scheme to http

--- internal/routes/proxy.go
+++ internal/routes/proxy.go
@@ -25,9 +25,12 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	upstreamHost := h.Upstreams.Strategy.Pick()
 	// Modify the original host with the chosen upstream
 	r.URL.Host = upstreamHost
+	// We need to set http or https on the request, only support http for now
+	r.URL.Scheme = "http"
 	res, err := h.Transport.RoundTrip(r)
 	if err != nil {
 		http.Error(w, "error occurred while performing roundtrip", http.StatusBadRequest)
+		return
 	}
 	defer res.Body.Close()
 	w.WriteHeader(http.StatusOK)
