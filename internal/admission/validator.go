c5faae72c63b6c76373a6bf0f26c3c94984a7db5
Max Celant <maxcelant@gmail.com>
2026-01-09 10:56:06 -0600

    feat: validating admission

--- internal/admission/validator.go
+++ internal/admission/validator.go
@@ -1,1 +1,101 @@
 package admission
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/maxcelant/sinkplot/internal/schema"
+)
+
+type validator struct {
+	app *schema.App
+}
+
+var validStrategies = map[string]bool{
+	"round-robin": true,
+	"random":      true,
+	"weighted":    true,
+}
+
+var validMatches = map[string]bool{
+	"prefix": true,
+	"exact":  true,
+	"regex":  true,
+}
+
+// Validate ensures that all necessary fields are set and are correct
+func Validate(app *schema.App) error {
+	d := validator{app}
+	if err := d.validatePorts(); err != nil {
+		return fmt.Errorf("port validation failed: %w", err)
+	}
+	if err := d.validateStrategy(); err != nil {
+		return fmt.Errorf("sink strategy validation failed: %w", err)
+	}
+	if err := d.validatePrefix(); err != nil {
+		return fmt.Errorf("route prefix validation failed: %w", err)
+	}
+	// Add localhost and hostnames as a possibility here
+	if err := d.validateIP(); err != nil {
+		return fmt.Errorf("upstream IP validation failed: %w", err)
+	}
+	// Validate the methods for all the routes
+	// Validate listener ports
+	// Validate that route -> sink are valid
+	// Validate route paths
+	return nil
+}
+
+func (v validator) validatePorts() error {
+	for _, s := range v.app.Sinks {
+		for _, u := range s.Upstreams {
+			if u.Port < 0 || u.Port > 65535 {
+				return fmt.Errorf("invalid port value %d", u.Port)
+			}
+		}
+	}
+	return nil
+}
+
+func (v validator) validateStrategy() error {
+	for _, s := range v.app.Sinks {
+		// This should never happen if you are running the defaulter first
+		if s.Strategy == nil || *s.Strategy == "" {
+			return fmt.Errorf("invalid strategy, value was null or blank")
+		}
+		if !validStrategies[*s.Strategy] {
+			return fmt.Errorf("invalid strategy %q", *s.Strategy)
+		}
+	}
+	return nil
+}
+
+func (v validator) validatePrefix() error {
+	for _, r := range v.app.Routes {
+		if r.Match == nil || *r.Match == "" {
+			return fmt.Errorf("invalid route match, value was null or blank")
+		}
+		if !validMatches[*r.Match] {
+			return fmt.Errorf("invalid route path matcher %q", *r.Match)
+		}
+	}
+	return nil
+}
+
+func (v validator) validateIP() error {
+	for _, s := range v.app.Sinks {
+		for _, u := range s.Upstreams {
+			if u.Address == "" {
+				return fmt.Errorf("upstream address cannot be empty in sink %q", s.Name)
+			}
+			if u.Address == "localhost" {
+				continue
+			}
+			// TODO: Validate hostnames as well
+			if ip := net.ParseIP(u.Address); ip == nil {
+				return fmt.Errorf("invalid IP address %q in sink %q", u.Address, s.Name)
+			}
+		}
+	}
+	return nil
+}
